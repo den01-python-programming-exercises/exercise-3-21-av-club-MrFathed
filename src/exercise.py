1d54a2159260a6d94917da66f7ce96faba7a850a
Justin Turner <jmturner638@gmail.com>
2021-08-02 10:13:43 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    while True:
+        line = input()
+
+        if line == '':
+            break
+
+        words = line.split()
+
+        for word in words:
+            if word.find('av') != -1:
+                print(word)
 
 if __name__ == '__main__':
     main()
